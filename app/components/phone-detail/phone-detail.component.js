17da8a2c0e404903d019a0644822fb8d48cbc042
Sergey <itsib.su@gmail.com>
2017-03-09 20:41:39 +0700

    Event Handlers

--- app/components/phone-detail/phone-detail.component.js
+++ app/components/phone-detail/phone-detail.component.js
@@ -6,8 +6,17 @@ component('phoneDetail', {
         function PhoneDetailController($routeParams, $http) {
             var self = this;
 
+            self.setImage = function setImage(imageUrl) {
+                self.mainImageUrl = imageUrl;
+            };
+
+            self.onDblclick = function onDblclick(imageUrl) {
+                alert('You double-clicked image: ' + imageUrl);
+            };
+
             $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
                 self.phone = response.data;
+                self.setImage(self.phone.images[0]);
                 console.log(response.data);
             });
         }
